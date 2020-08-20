2ec98099f3739bc35cc887a6b1cb12b1d3ca5dff
vladimirborodin <boberneprotiv@gmail.com>
2020-08-20 13:41:37 +0300

    Configure swagger

--- GhostNetwork.Publications/Startup.cs
+++ GhostNetwork.Publications/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace GhostNetwork.Publications
 {
@@ -8,11 +10,30 @@ namespace GhostNetwork.Publications
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "GhostNetwork/Publications API",
+                    Version = "1.0.0"
+                });
+            });
+            
             services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app
+                    .UseSwagger()
+                    .UseSwaggerUI(config =>
+                    {
+                        config.SwaggerEndpoint("/swagger/v1/swagger.json", "Relations.API V1");
+                    });
+            }
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
