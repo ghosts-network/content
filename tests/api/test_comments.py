a1a55eb7ab112208e247a11d34368009a906632f
vladimirborodin <boberneprotiv@gmail.com>
2020-10-02 21:04:46 +0300

    fix code style

--- tests/api/test_comments.py
+++ tests/api/test_comments.py
@@ -8,30 +8,30 @@ class TestComments(Api):
   
   def test_get_comment_by_id(self):
     publication_resp = self.post_publication({'content': 'My first publication #awesome'})
-    publicationId = publication_resp.json()['id']
+    publication_id = publication_resp.json()['id']
 
-    comment_resp = self.post_comment({'publicationId': publicationId, 'content': 'comment for the first publication'})
-    commentId = comment_resp.json()['id']
+    comment_resp = self.post_comment({'publicationId': publication_id, 'content': 'comment for the first publication'})
+    comment_id = comment_resp.json()['id']
 
-    resp = self.get_comment_by_id(commentId)
+    resp = self.get_comment_by_id(comment_id)
     resp_body = resp.json()
 
     assert resp.status_code == 200
 
   def test_get_comments_by_publication(self):
     publication_resp = self.post_publication({'content': 'My first publication #awesome'})
-    publicationId = publication_resp.json()['id']
+    publication_id = publication_resp.json()['id']
 
-    comment_resp = self.post_comment({'publicationId': publicationId, 'content': 'comment for the first publication'})
-    commentId = comment_resp.json()['id']
+    comment_resp = self.post_comment({'publicationId': publication_id, 'content': 'comment for the first publication'})
+    comment_id = comment_resp.json()['id']
     
-    resp = self.get_comments_by_publication_id(publicationId)
+    resp = self.get_comments_by_publication_id(publication_id)
     resp_body = resp.json()
 
     assert resp.status_code == 200
     assert len(resp_body) == 1
-    assert resp_body[0]['id'] == commentId
-    assert resp_body[0]['publicationId'] == publicationId
+    assert resp_body[0]['id'] == comment_id
+    assert resp_body[0]['publicationId'] == publication_id
     assert resp_body[0]['content'] == 'comment for the first publication'
 
   def test_get_comment_by_nonexistent_publication(self):
@@ -41,9 +41,9 @@ class TestComments(Api):
 
   def test_post_comment(self):
     publication_resp = self.post_publication({'content': 'My first publication #awesome'})
-    publicationId = publication_resp.json()['id']
+    publication_id = publication_resp.json()['id']
 
-    resp = self.post_comment({'publicationId': publicationId, 'content': 'comment for the first publication'})
+    resp = self.post_comment({'publicationId': publication_id, 'content': 'comment for the first publication'})
     resp_body = resp.json()
 
     assert resp.status_code == 201
