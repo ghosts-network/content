31702c21ed5139327514c501fd8bae7a7654e167
vladimirborodin <boberneprotiv@gmail.com>
2020-10-02 20:59:04 +0300

    fix comment delete

--- tests/api/src/api.py
+++ tests/api/src/api.py
@@ -38,9 +38,8 @@ class Api:
 
     return requests.get(url, headers=headers)
 
-  def delete_comment(id):
+  def delete_comment(self, id):
     url = 'http://localhost:5300/comments/' + id
     headers = {'Content-Type': 'application/json' }
     
     return requests.delete(url, headers=headers)
-    
