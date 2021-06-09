60e43d8bf9c11b11497cdb22460f11dfc1e8baae
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-06-09 17:46:50 +0300

    add comparator&add deserialize method

--- tests/GhostNetwork.Content.UnitTest/TestServerHelper.cs
+++ tests/GhostNetwork.Content.UnitTest/TestServerHelper.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Net.Http;
 using System.Text;
-using System.Text.Json;
+using System.Threading.Tasks;
 using GhostNetwork.Content.Api;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace GhostNetwork.Content.UnitTest
 {
@@ -22,7 +23,12 @@ namespace GhostNetwork.Content.UnitTest
 
         public static StringContent AsJsonContent<T>(this T input)
         {
-            return new StringContent(JsonSerializer.Serialize(input), Encoding.Default, "application/json");
+            return new StringContent(JsonConvert.SerializeObject(input), Encoding.Default, "application/json");
+        }
+
+        public static async Task<T> DeserializeContent<T>(this HttpContent content)
+        {
+            return JsonConvert.DeserializeObject<T>(await content.ReadAsStringAsync());
         }
     }
 }
