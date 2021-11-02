0dbaea71fc3e1d4e958663dd7f0811b987d6b841
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-11-02 12:28:54 +0200

    add new case to commentUpdateTests

--- tests/GhostNetwork.Content.UnitTest/Comments/API/UpdateTests.cs
+++ tests/GhostNetwork.Content.UnitTest/Comments/API/UpdateTests.cs
@@ -14,8 +14,8 @@ namespace GhostNetwork.Content.UnitTest.Comments.Api
     [TestFixture]
     public class UpdateTests
     {
-      [Test]
-      public async Task Update_NoContent()
+        [Test]
+        public async Task Update_NoContent()
         {
             // Setup
             const string commentId = "someId";
@@ -44,8 +44,8 @@ namespace GhostNetwork.Content.UnitTest.Comments.Api
             Assert.AreEqual(response.StatusCode, HttpStatusCode.NoContent);
         }
 
-      [Test]
-      public async Task Update_BadRequest_EmptyContent()
+        [Test]
+        public async Task Update_BadRequest_EmptyContent()
         {
             // Setup
             const string commentId = "someId";
@@ -69,8 +69,8 @@ namespace GhostNetwork.Content.UnitTest.Comments.Api
             Assert.AreEqual(response.StatusCode, HttpStatusCode.BadRequest);
         }
 
-      [Test]
-      public async Task Update_NotFound()
+        [Test]
+        public async Task Update_NotFound()
         {
             // Setup
             const string commentId = "someId";
@@ -94,6 +94,51 @@ namespace GhostNetwork.Content.UnitTest.Comments.Api
             var response = await client.PutAsync($"comments/{commentId}", model.AsJsonContent());
 
             Assert.AreEqual(response.StatusCode, HttpStatusCode.NotFound);
+        }
+
+        [Test]
+        public async Task Update_AllowTimeToEdit_Expired_BadRequest()
+        {
+            // Setup
+            const string commentId = "some_id";
+            const string publicationId = "some_id";
+            const string content = "some content";
+
+            var comment = new Comment(
+                commentId,
+                content,
+                DateTimeOffset.UtcNow.AddDays(-1),
+                publicationId,
+                replyCommentId: null,
+                new UserInfo(Guid.Empty, "Some Name", null));
+
+            var input = new UpdateCommentModel
+            {
+                Content = "new content"
+            };
+
+            var serviceMock = new Mock<ICommentsService>();
+            var storageMock = new Mock<ICommentsStorage>();
+
+            serviceMock
+                .Setup(s => s.GetByIdAsync(commentId))
+                .ReturnsAsync(comment);
+
+            serviceMock
+                .Setup(s => s.UpdateAsync(commentId, input.Content))
+                .ReturnsAsync(DomainResult.Error("API error!"));
+
+            var client = TestServerHelper.New(collection =>
+            {
+                collection.AddScoped(provider => serviceMock.Object);
+                collection.AddScoped(provider => storageMock.Object);
+            });
+
+            // Act
+            var response = await client.PutAsync($"/comments/{commentId}/", input.AsJsonContent());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
         }
     }
 }
