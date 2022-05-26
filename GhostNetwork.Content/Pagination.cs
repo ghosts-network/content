913f4877e99716e1d90d5d79eea15cb4c80729a6
boberneprotiv <boberneprotiv@gmail.com>
2022-05-26 20:18:47 +0300

    Clear-up pagination object

--- GhostNetwork.Content/Pagination.cs
+++ GhostNetwork.Content/Pagination.cs
@@ -1,8 +1,3 @@
 namespace GhostNetwork.Content;
 
-public record Pagination(string Cursor, int Limit, int Skip)
-{
-    public static Pagination ByCursor(string cursor, int limit) => new Pagination(cursor, limit, default);
-
-    public static Pagination BySkip(int skip, int limit) => new Pagination(default, limit, skip);
-}
+public record Pagination(string Cursor, int Limit, int Skip);
